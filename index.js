1b713be4aa63c7736821b051aae6c38bfd261f39
Ragan Hagey <raganahagey@gmail.com>
2022-09-18 12:27:51 -0400

    commit tree 2c60d9a2bc8963ecffaeae0b498964879ae135fb

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31
+
+let num2 = 2
+
+let multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10)
+
+let max = Math.max(1, 2, 3, 4, 5, 20)
+
+let num3 = 9
+
+let num4 = 22
+
+let mod = num4 % num3
